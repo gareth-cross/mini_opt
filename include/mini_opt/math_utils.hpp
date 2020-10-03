5b0fdb548864c413e28692de827b9f45530fbecf
gareth-cross <gcross.code@icloud.com>
2020-10-03 14:56:46 -0700

    add mod pi func

---
+++ include/mini_opt/math_utils.hpp
@@ -0,0 +1,22 @@
+// Copyright 2020 Gareth Cross
+#pragma once
+#include <cmath>
+#include <type_traits>
+
+namespace mini_opt {
+
+// Wrap into range of [-pi, pi].
+template <typename Scalar>
+Scalar ModPi(Scalar x) {
+  constexpr auto pi = static_cast<Scalar>(M_PI);
+  if (x < 0) {
+    return -ModPi(-x);
+  }
+  const Scalar x_mod_2pi = std::fmod(x, 2 * pi);
+  if (x_mod_2pi > pi) {
+    return x_mod_2pi - 2 * pi;
+  }
+  return x_mod_2pi;
+}
+
+}  // namespace mini_opt
---
+++ test/math_utils_test.cc
@@ -0,0 +1,30 @@
+// Copyright 2020 Gareth Cross
+#include "mini_opt/math_utils.hpp"
+
+#include "test_utils.hpp"
+
+namespace mini_opt {
+
+TEST(MathUtilsTest, TestModPi) {
+  ASSERT_EQ(0, ModPi(0));
+  ASSERT_EQ(M_PI / 3, ModPi(M_PI / 3));
+  ASSERT_EQ(M_PI / 4, ModPi(M_PI / 4));
+  ASSERT_NEAR(3 * M_PI / 4, ModPi(3 * M_PI / 4), tol::kPico);
+  ASSERT_NEAR(3 * M_PI / 4, ModPi(3 * M_PI / 4 + 2 * M_PI), tol::kPico);
+  ASSERT_NEAR(3 * M_PI / 4, ModPi(3 * M_PI / 4 + 4 * M_PI), tol::kPico);
+  ASSERT_NEAR(0.0, ModPi(2 * M_PI), tol::kPico);
+  ASSERT_NEAR(0.0, ModPi(4 * M_PI), tol::kPico);
+  ASSERT_NEAR(0.0, ModPi(12 * M_PI), tol::kPico);
+  ASSERT_NEAR(M_PI, ModPi(3 * M_PI), tol::kPico);
+  ASSERT_NEAR(M_PI, ModPi(7 * M_PI), tol::kPico);
+  ASSERT_NEAR(-3 * M_PI / 4, ModPi(5 * M_PI + M_PI / 4), tol::kPico);
+  ASSERT_NEAR(-M_PI / 2, ModPi(5 * M_PI + M_PI / 2), tol::kPico);
+  ASSERT_EQ(-0.0, ModPi(-0.0));
+  ASSERT_EQ(-M_PI / 6, ModPi(-M_PI / 6));
+  ASSERT_EQ(-M_PI / 2, ModPi(-M_PI / 2));
+  ASSERT_NEAR(-3 * M_PI / 4, ModPi(-3 * M_PI / 4 - 2 * M_PI), tol::kPico);
+  ASSERT_EQ(-M_PI, ModPi(-M_PI));
+  ASSERT_NEAR(0.0, ModPi(-4 * M_PI), tol::kPico);
+}
+
+}  // namespace mini_opt
