cb6db6d09c87b15b43cb264168d9e13f7375714f
gareth-cross <gcross.code@icloud.com>
2021-10-27 18:20:48 -0700

    Add install options to cmake, add eigen install option

--- cmake/FindEigen.cmake
+++ cmake/FindEigen.cmake
@@ -9,25 +9,37 @@ list(APPEND EIGEN_CHECK_INCLUDE_DIRS
 # Check if the user specified a location w/ -D, and look there as well.
 if (DEFINED EIGEN_DIRECTORY)
   list(APPEND EIGEN_CHECK_INCLUDE_DIRS ${EIGEN_DIRECTORY})
-endif(DEFINED EIGEN_DIRECTORY)
+endif (DEFINED EIGEN_DIRECTORY)
 
 list(APPEND EIGEN_CHECK_PATH_SUFFIXES
-     eigen3)
+    eigen3)
 
 set(EIGEN_SIGNATURE signature_of_eigen3_matrix_library)
 
 find_path(EIGEN_INCLUDE_DIR
-        NAMES ${EIGEN_SIGNATURE}
-        PATHS ${EIGEN_CHECK_INCLUDE_DIRS}
-        PATH_SUFFIXES ${EIGEN_CHECK_PATH_SUFFIXES}
-        NO_DEFAULT_PATH)
+    NAMES ${EIGEN_SIGNATURE}
+    PATHS ${EIGEN_CHECK_INCLUDE_DIRS}
+    PATH_SUFFIXES ${EIGEN_CHECK_PATH_SUFFIXES}
+    NO_DEFAULT_PATH)
 
-if(NOT EIGEN_INCLUDE_DIR OR NOT EXISTS ${EIGEN_INCLUDE_DIR})
+if (NOT EIGEN_INCLUDE_DIR OR NOT EXISTS ${EIGEN_INCLUDE_DIR})
   set(EIGEN_FOUND FALSE)
   message(WARNING "Failed to locate Eigen.")
-else()
+else ()
   set(EIGEN_FOUND TRUE)
   message(STATUS "Eigen found: ${EIGEN_INCLUDE_DIR}")
-endif()
+endif ()
 
 set(EIGEN_INCLUDE_DIRS ${EIGEN_INCLUDE_DIR})
+
+# Create a target for eigen w/ the correct interface include directories.
+# See https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
+if (${EIGEN_FOUND})
+  if (NOT TARGET Eigen::Eigen)
+    message(STATUS "Creating target for Eigen: Eigen::Eigen")
+    add_library(Eigen::Eigen INTERFACE IMPORTED)
+    set_target_properties(Eigen::Eigen PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${EIGEN_INCLUDE_DIRS}"
+        )
+  endif ()
+endif ()
