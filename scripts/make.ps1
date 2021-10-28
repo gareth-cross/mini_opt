be5cdefe517e1b6588fbb64af18dce987eade941
gareth-cross <gcross.code@icloud.com>
2021-10-27 21:04:13 -0700

    Add build powershell script

--- scripts/make.ps1
+++ scripts/make.ps1
@@ -1,4 +1,15 @@
-$build_config = $args[0]
-$build_cmd = "MSBuild.exe `"mini_opt.sln`" -nologo -verbosity:m -property:Configuration=$build_config"
+param (
+    [string]$build_type='Release',
+    [string]$verbosity='m',
+    [switch]$install
+)
+$build_cmd = "MSBuild.exe ALL_BUILD.vcxproj -nologo -verbosity:$verbosity -property:Configuration=$build_type"
 Write-host "Invoking: $build_cmd"
 Invoke-Expression $build_cmd
+
+if ($install) {
+    # build succeeded, run the install target
+    $install_cmd = "MSBuild.exe INSTALL.vcxproj -verbosity:$verbosity -property:Configuration=$build_type"
+    Write-host "Invoking: $install_cmd"
+    Invoke-Expression $install_cmd
+}
